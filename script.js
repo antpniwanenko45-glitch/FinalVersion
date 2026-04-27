8eb86b919d5332ca689415cf97eb08ef3db82dac
antpniwanenko45-glitch <antpniwanenko45@gmail.com>
2026-04-27 21:28:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -203,3 +203,9 @@ I genuinely feel like they would fit perfectly...
     }
   }
 }
+const textarea = document.querySelector(".input-inner textarea");
+
+textarea.addEventListener("input", () => {
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + "px";
+});
